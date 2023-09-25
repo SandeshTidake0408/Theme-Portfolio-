d88279d903e7da56c4d16314ffd49d86f4e4c837
SandeshTidake0408 <sandeshtidake37@gmail.com>
2023-09-26 00:00:45 +0530

    adding media query

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
-    fixedClass: "is-fixed",
+    multiplier: 1,
 });
 
 function landingTextAnimation() {
